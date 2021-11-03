7f65cdcd514b7431b1d2f80c6b161758b5b79384
Nicolas Alonso <63936366+n-alonso@users.noreply.github.com>
2021-11-03 23:14:14 +0200

    Update convertToBaby.js

--- convertToBaby.js
+++ convertToBaby.js
@@ -1,4 +1,5 @@
-/* Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '.  
+/* Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string  
+in the array prepended with 'baby '.  
 Note: You may have noticed how convenient it would be to use built-in methods, but you’re not allowed to use them here.  */
 
 // Write your code here:
