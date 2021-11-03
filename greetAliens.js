7a715d45360fc7918304ec948e6398795ee34261
Nicolas Alonso <63936366+n-alonso@users.noreply.github.com>
2021-11-03 15:29:53 +0200

    Create greetAliens.js

---
+++ greetAliens.js
@@ -0,0 +1,17 @@
+/* Write a function, greetAliens(), that takes in an array of strings and uses a for loop to print a greeting with each string in the array.  
+The greeting should take the following format:  
+“Oh powerful [stringElement], we humans offer our unconditional surrender!”  
+Note: You may have noticed how convenient it would be to use .forEach(), but you’re not allowed to use it here. */
+
+// Write your code here:
+const greetAliens = array => {
+  for (let alien of array) {
+    return `Oh powerful ${alien}, we humans offer our unconditional surrender!`
+  }
+}
+
+// When you're ready to test your code, uncomment the below and run:
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+console.log(greetAliens(aliens));
